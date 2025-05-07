373dde29ac09505b6f90b86a4ab575e8fd6e8908
Ric <henriqueft04@ua.pt>
2025-05-07 19:26:08 +0100

    Fix icon issues and update theme to white and blue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://henriqueft04.github.io",
   base: "/henriqueteixeira.pwp",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind()],
 });
